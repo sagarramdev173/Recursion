3b7f1cd304638c4b2bbf89d8e827c4b35df56781
sagar <sagar.ramdev173@gmail.com>
2020-07-31 18:31:31 +0530

    improving hanoi problem code

--- tower_of_hanoi.cpp
+++ tower_of_hanoi.cpp
@@ -6,10 +6,12 @@ void solve(int source,int destination,int helper,int no_of_plates)
     if(no_of_plates==1) //base condition
     {
         cout<<"noving plate "<<no_of_plates<<" from "<<source<<" to "<<destination<<endl;
+        no_of_plates--;
         return;
     }
     solve(source,helper,destination,no_of_plates-1);  // hypthesis
     cout<<"noving plate "<<no_of_plates<<"from "<<source<<" to "<<destination<<endl; //induction
+    no_of_plates--;
     solve(helper,destination,source,no_of_plates); // induction
     return;
 
@@ -17,7 +19,7 @@ void solve(int source,int destination,int helper,int no_of_plates)
 
 int main(int argc, char const *argv[])
 {
-    int no_of_plates=2;
+    int no_of_plates=4;
     int source=1;
     int destination=3;
     int helper=2;
